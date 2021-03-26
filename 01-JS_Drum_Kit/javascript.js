071ac3bfc8aaf95cbdc42fc0e621b5a7f6da2087
allan boodhun <allan.boodhun@gmail.com>
2021-03-27 00:21:36 +0100

    sound ok

--- 01-JS_Drum_Kit/javascript.js
+++ 01-JS_Drum_Kit/javascript.js
@@ -1,3 +1,10 @@
 document.addEventListener('keydown',function(e) {
-  const audio = document.querySelector('audio')
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //select the audio file from keydown
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //select the div from keydown
+  console.log(key)
+
+  if (!audio) return; // stop the function if no song found
+  audio.currentTime = 0; // set the timer to 0. Now i can spam
+  audio.play(); // play the constant song
+  
 });
