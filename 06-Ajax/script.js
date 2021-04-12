a82c21ab00fc44c84d52d7d9933340cb68f8c382
allanboodhun <allan.boodhun@gmail.com>
2021-04-12 14:22:58 +0200

    complete challenge 6

--- 06-Ajax/script.js
+++ 06-Ajax/script.js
@@ -1,1 +1,43 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint)
+  .then(data => data.json())
+  .then(jdata => cities.push(...jdata));
+
+console.log(cities);
+
+function citiesFinder (wordToMatch, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi'); // On défini la regex à partir de wordtomatch. 'gi' pour "global insensitive"
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+
+function displayMatches() {
+  const matchArray = citiesFinder(this.value, cities);
+  const html = matchArray.map(place => {
+    // pour avoir la regex cherchée en surbrillance
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);// pour avoir la regex cherchée en surbrillance
+    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);// pour avoir la regex cherchée en surbrillance
+    return `
+    <li>
+      <span class ="name">${cityName}, ${stateName}</span>
+      <span class ="population">${numberWithCommas(place.population)}</span>
+    </li>`;
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change',displayMatches);
+searchInput.addEventListener('keyup',displayMatches);
